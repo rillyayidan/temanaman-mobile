d800c5de2f617bf603240a37fc10427cc428552a
rillyayidan <zidanrilly09@gmail.com>
2025-12-30 20:12:19 +0700

    1.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.temanaman.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
